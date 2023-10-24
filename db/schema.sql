82e2bcfd1226f198e4a44c5e467307b75a35d86f
Riley <rileyg0035@gmail.com>
2023-10-24 19:00:44 -0300

    started adding tables needed

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,19 @@
-
+-- Department -- 
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+-- Role --
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) 
+);
+
+
+
