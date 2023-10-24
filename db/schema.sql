9d56c982ecba80c1470f6e0d6c79f48dfeeaaa86
Riley <rileyg0035@gmail.com>
2023-10-24 19:06:53 -0300

    added quieries.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
--- Department -- 
+-- Department Table -- 
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30)
 );
 
--- Role --
+-- Role Table --
 
 CREATE TABLE role (
     id INT PRIMARY KEY,
@@ -17,3 +17,14 @@ CREATE TABLE role (
 
 
 
+-- Employee Table -- 
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT, 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
