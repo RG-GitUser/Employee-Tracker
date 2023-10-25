f0b2ed74a95bc13b091d6efb7470faada40f7fe7
Riley <rileyg0035@gmail.com>
2023-10-25 18:39:37 -0300

    added database function for sql queries

--- js/database.js
+++ js/database.js
@@ -1,14 +1,32 @@
-const msysql = require('mysql');
+const mysql = require('mysql');
 
-// connection pool - reuses database connection
-
+// Connection pool - reusing database connection
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: '',
-    user:'',
-    password:'',
-    database: '',
+  connectionLimit: 10, 
+  host: 'your_database_host',
+  user: 'your_database_user',
+  password: 'your_database_password',
+  database: 'your_database_name',
 });
 
-// Function for SQL queries
+// Function for sql queries
+function query(sql, values) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        return reject(err);
+      }
+      connection.query(sql, values, (err, results) => {
+        connection.release();
+        if (err) {
+          return reject(err);
+        }
+        resolve(results);
+      });
+    });
+  });
+}
 
+module.exports = {
+  query, 
+};
