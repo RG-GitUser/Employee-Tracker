2d51d5649ae8414e328198577bbf8c8d6a7bad4e
RG-GitUser <rileygaffney3@icloud.com>
2024-01-11 12:31:16 -0400

    making adjustments to server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'your_password',
+  password: 'UNBbootcamp!23',
   database: 'employee_db',
 });
 
